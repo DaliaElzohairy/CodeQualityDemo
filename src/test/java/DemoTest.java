95739c213ada51aced8f90cbb2d56249d594e1f7
Dalia.ElZohairy <dalia.elzohairy@linkdev.com>
2024-10-30 13:45:25 +0300

    tryJacoco

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -1,4 +1,16 @@
+import org.example.Demo;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DemoTest {
 
+    private Demo calculator = new Demo();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(0, calculator.add(-1, 1));
+        assertEquals(-3, calculator.add(-1, -2));
+    }
 }
